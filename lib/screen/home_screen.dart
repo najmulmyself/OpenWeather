1afb7b83f6480523ca96a6ad03f2af5fa3aa5407
najmulmyself <najmul.myself@gmail.com>
2022-03-30 22:47:18 +0600

    need to understand what is 'instance of users'

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:openweather/modal/test.dart';
 
-class HomeScreen extends StatelessWidget {
+const apiKey = 'c1478dff46b3220baeb2e7127cb080f4';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  getData() async {
+    // Future Response = await http.get(Uri.parse('htstp://api.openweathermap.org/data/2.5/forecast?id=524901&appid={apiKey}'));
+
+    // Test Api
+
+    http.Response response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/users'),
+    );
+    var test = usersFromJson(response.body);
+    print(test);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +50,7 @@ class HomeScreen extends StatelessWidget {
                 Text('10C'),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
