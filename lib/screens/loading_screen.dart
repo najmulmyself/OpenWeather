de0aaacb12871961dd83c54403595859360f516d
najmulmyself <najmul.myself@gmail.com>
2022-07-19 01:28:28 +0600

    problem with getting data,loading inf.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:openweather/screens/location_screen.dart';
 import 'package:openweather/services/location.dart';
 import 'package:openweather/services/networking.dart';
 
-final apiKey = 'c1478dff46b3220baeb2e7127cb080f4';
+const apiKey = 'c1478dff46b3220baeb2e7127cb080f4';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -28,21 +32,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     Networkhelper networkhelper = Networkhelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     var weatherData = await networkhelper.getData();
+    print(latitude);
+    print(longitude);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => LocationScreen(),
+      ),
+    );
   }
-
-  void getData() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // no  need to call the function;
-            // getLocation();
-          },
-          child: Text('Get Location'),
-        ),
+        child: SpinKitSpinningLines(color: Colors.red, size: 100.0),
       ),
     );
   }
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -2,14 +2,15 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Networkhelper {
+  final url;
   Networkhelper(this.url);
-  final  url;
 
   Future getData() async {
     http.Response response = await http.get(url);
 
     if (response.statusCode == 200) {
       String data = response.body;
+      print(data);
       return jsonDecode(data);
     } else {
       print(response.statusCode);
